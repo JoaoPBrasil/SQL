6167d49b0fc5852db1ff108797d2d87aa64e91be
João Pedro Brasil <116403431+JoaoPBrasil@users.noreply.github.com>
2024-12-10 19:47:33 -0300

    Create Desafio29.sql

---
+++ Exercises/Desafio29.sql
@@ -0,0 +1,16 @@
+-- UPDATE:
+
+UPDATE nomeTabela
+SET 	coluna1 = valor1
+coluna2 = valor2
+WHERE condição
+
+-- Desafio:
+
+UPDATE aula
+SET nome = 'outroTeste'
+WHERE id = 4;
+
+UPDATE aula
+SET nome = 'Eu'
+WHERE id = 1;
