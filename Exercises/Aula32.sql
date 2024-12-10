88a025d307a0fcdca3e9991d27b1b41ad684c0c5
João Pedro Brasil <116403431+JoaoPBrasil@users.noreply.github.com>
2024-12-10 19:48:33 -0300

    Create Aula32.sql

---
+++ Exercises/Aula32.sql
@@ -0,0 +1,16 @@
+-- DROP:
+
+DROP TABLE NomeTabela; (Deletar tabela)
+
+TRUNCATE TABEL NomeTabela; (Excluir dados das tabelas)
+
+-- Desafio:
+
+CREATE TABLE DadosNovos (
+Id INT PRIMARY KEY,
+Nome VARCHAR(150),
+Sobrenome VARCHAR(150),
+Idade INT NOT NULL
+);
+
+DROP TABLE DadosNovos;
