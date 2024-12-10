b36e9c1bd2961acff32a52cbdd94fcfc5c500479
João Pedro Brasil <116403431+JoaoPBrasil@users.noreply.github.com>
2024-12-10 19:38:03 -0300

    Create Desafio05.sql

---
+++ Exercises/Desafio05.sql
@@ -0,0 +1,20 @@
+SELECT COUNT (*)
+FROM tabela;
+
+SELECT COUNT (DISTINCT coluna1)
+FROM tabela;
+
+-- Desafio 1:
+
+SELECT COUNT(*)
+FROM Production.Product;
+
+-- Desafio 2:
+
+SELECT COUNT(Size)
+FROM Production.Product;
+
+-- Desafio 3:
+
+SELECT COUNT(DISTINCT Size)
+FROM Production.Product;
