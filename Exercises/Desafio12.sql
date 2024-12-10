8148932f38e384d721701964e11bb00bb85cb55f
João Pedro Brasil <116403431+JoaoPBrasil@users.noreply.github.com>
2024-12-10 19:40:37 -0300

    Create Desafio12.sql

---
+++ Exercises/Desafio12.sql
@@ -0,0 +1,11 @@
+SELECT TOP 10 sum(linetotal) as "Soma"
+FROM Sales.SalesOrderDetail;
+
+SELECT TOP 10 Min(linetotal)
+FROM Sales.SalesOrderDetail;
+
+SELECT TOP 10 Max(linetotal)
+FROM Sales.SalesOrderDetail;
+
+SELECT TOP 10 Avg(linetotal)
+FROM Sales.SalesOrderDetail;
