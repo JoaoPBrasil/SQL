6bfcd6ddb10a3b6ccd4b5608bc878026c1010811
João Pedro Brasil <116403431+JoaoPBrasil@users.noreply.github.com>
2024-12-10 19:45:15 -0300

    Create Desafio23.sql

---
+++ Exercises/Desafio23.sql
@@ -0,0 +1,20 @@
+-- SUBQUERIES:
+
+SELECT *
+FROM Production.Product
+WHERE ListPrice > (SELECT AVG(ListPrice) FROM Production.Product);
+
+SELECT FirstName
+FROM Person.Person
+WHERE BusinessEntityID IN (SELECT BusinessEntityID FROM HumanResources.Employee WHERE JobTitle = 'Design Engineer');
+
+SELECT FirstName
+FROM Person.Person P
+INNER JOIN HumanResources.Employee E ON P.BusinessEntityID = E.BusinessEntityID
+AND E.JobTitle = 'Design Engineer';
+
+-- Desafio:
+
+SELECT *
+FROM Person.Address
+WHERE StateProvinceID IN (SELECT StateProvinceID FROM Person.StateProvince WHERE Name = 'Alberta');
