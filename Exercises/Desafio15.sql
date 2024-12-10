f7f08fc144f6dde909da8ebcc77b76082e8dace4
João Pedro Brasil <116403431+JoaoPBrasil@users.noreply.github.com>
2024-12-10 19:41:57 -0300

    Create Desafio15.sql

---
+++ Exercises/Desafio15.sql
@@ -0,0 +1,19 @@
+SELECT TOP 10 ListPrice as "Preco do Produto"
+From Production.Product;
+
+SELECT TOP 10 AVG(ListPrice) as "Preco Medio"
+From Production.Product;
+
+--Desafios:
+
+SELECT FirstName as "Primeiro Nome"
+FROM Person.Person
+
+SELECT LastName as "Sobrenome"
+FROM Person.Person;
+
+SELECT ProductNumber as "Numero do Produto"
+FROM Production.Product;
+
+SELECT UnitPrice as "´Preço Unitário"
+FROM Sales.SalesOrderDetail;
