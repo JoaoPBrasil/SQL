85967835d4174b49d190e19639bfdc7ebc41f92c
João Pedro Brasil <116403431+JoaoPBrasil@users.noreply.github.com>
2024-12-10 19:49:00 -0300

    Create Aula33.sql

---
+++ Exercises/Aula33.sql
@@ -0,0 +1,23 @@
+-- CHECK CONSTRAINT:
+
+CREATE TABLE CarteiraMotorista(
+Id INT NOT NULL,
+Nome VARCHAR(255) NOT NULL,
+Idade INT CHECK (Idade >= 18)
+);
+
+INSERT INTO CarteiraMotorista(id, nome, idade) VALUES (1, 'exemplo', 17)
+
+-- Desafio:
+
+CREATE TABLE BebidaAlcoolica(
+Id INT NOT NULL,
+Nome VARCHAR(255) NOT NULL,
+Idade INT CHECK (Idade >= 21)
+);
+
+CREATE TABLE Tatuagem(
+Id INT NOT NULL,
+Nome VARCHAR(255) NOT NULL,
+Idade INT CHECK (Idade >= 16)
+);
